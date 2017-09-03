e3bcddb1bc3c10b719a2b07c8350b71b74691945
Robert Dickerson <rdickeriii@gmail.com>
2017-09-03 15:37:25 -0700

    Refactor: fun mit numbers
    end_of_line

---
+++ numbers.py
@@ -0,0 +1,27 @@
+print(3 + 1)
+print(1 + 2 + 3 * 3)
+print((1 + 2 + 3) * 3)
+print(3 ** 2)
+print(9 % 2)
+print(15 // 4)
+print(16 // 4)
+print(-5 // 4)
+
+# Write an expression that calculates the average of 23, 32 and 64.
+# Place the expression in this print statement.
+print((23 + 32 + 64) / 3)
+
+
+# In this quiz you're going to do some calculations for a tiler. Two parts of a floor need tiling.
+# One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles long.
+#  Tiles come in packages of 6.
+#
+# How many tiles are needed?
+# You buy 17 packages of tiles containing 6 tiles each.
+# How many tiles will be left over?
+
+# Fill this in with an expression that calculates how many tiles are needed.
+print((9*7)+(5*7))
+
+# Fill this in with an expression that calculates how many tiles will be left over.
+print((17*6)-(9*7+5*7))
