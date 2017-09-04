b9a54096604503d72e09039725b633b763846704
Robert Dickerson <rdickeriii@gmail.com>
2017-09-04 14:33:14 -0700

    Refactor: Quiz Time. Naled It.
    end_of_line

--- if.py
+++ if.py
@@ -70,3 +70,16 @@ else:
     ticket_price = adult_ticket
 message = "Somebody who is {} years old will pay ${} to ride the bus.".format(age,ticket_price)
 print(message)
+
+def which_prize(points):
+    """
+    Returns the prize-winning message, given a number of points
+    """
+    if points <= 50:
+        return "Congratulations! You have won a wooden rabbit!"
+    elif points <= 150:
+        return "Oh dear, no prize this time."
+    elif points <= 180:
+        return "Congratulations! You have won a wafer-thin mint!"
+    else:
+        return "Congratulations! You have won a penguin!"
