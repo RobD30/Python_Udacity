4459718fb151d6b5766ca96cacc9b93bfc773281
Robert Dickerson <rdickeriii@gmail.com>
2017-09-04 12:24:13 -0700

    Refactor: Quiz Time. Nailed It.
    end_of_line

--- strings.py
+++ strings.py
@@ -2,3 +2,22 @@ print("Hello World")
 print('Even More Hello World')
 welcome_message = "Hello, welcome to Udacity!"
 print(welcome_message)
+instructor_1 = "Philip"
+instructor_2 = 'Charlie'
+print(instructor_1 + instructor_2)
+print(instructor_1 + " and " + instructor_2)
+
+
+pet_halibut = 'Why should I be tarred with the epithet "loony" merely because I have a pet halibut?'
+salesman = '"I think you\'re an encyclopaedia salesman"'
+messiah = 'He\'s not the Messiah, he\'s a very naughty boy!'
+print(messiah)
+
+
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+# The message should be use the same format as this one:
+# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+print(username + " accessed the site " + url + " at " + timestamp + ".")
