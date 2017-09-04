46d78db2556dd83459515cbb0b94183bdb539eb2
Robert Dickerson <rdickeriii@gmail.com>
2017-09-04 11:42:10 -0700

    Refactor: Quiz time.
    end_of_line

--- variables.py
+++ variables.py
@@ -2,19 +2,38 @@
 
 
 
-manila_pop = 1788573
-manila_area = 16.56
-manila_pop_density = manila_pop / manila_area
-print(manila_area)
-print(manila_pop_density)
+# manila_pop = 1788573
+# manila_area = 16.56
+# manila_pop_density = manila_pop / manila_area
+# print(manila_area)
+# print(manila_pop_density)
+#
+#
+# manila_pop += 1675 # increase the value of manila_pop by 1675
+# print(manila_pop)
+# manila_pop -= 250 # decrease the value of manila_pop by 250
+# print(manila_pop)
+# manila_pop *= 0.9 # decimate manila_pop
+# print(manila_area)
+# manila_area /=  2 # approximate the female population of Manila
+# print(manila_area)
+# print(manila_pop)
 
+# The current volume of a water reservoir (in cubic metres)
+reservoir_volume = 4.445e8
+# The amount of rainfall from a storm (in cubic metres)
+rainfall = 5e6
 
-manila_pop += 1675 # increase the value of manila_pop by 1675
-print(manila_pop)
-manila_pop -= 250 # decrease the value of manila_pop by 250
-print(manila_pop)
-manila_pop *= 0.9 # decimate manila_pop
-print(manila_area)
-manila_area /=  2 # approximate the female population of Manila
-print(manila_area)
-print(manila_pop)
+# decrease the rainfall variable by 10% to account for runoff
+
+# add the rainfall variable to the reservoir_volume variable
+
+# increase reservoir_volume by 5% to account for stormwater that flows
+# into the reservoir in the days following the storm
+
+# decrease reservoir_volume by 5% to account for evaporation
+
+# subtract 2.5e5 cubic metres from reservoir_volume to account for water
+# that's piped to arid regions.
+
+# print the new value of the reservoir_volume variable
