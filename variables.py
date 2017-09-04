38905c3c9ed156195474a125d9005b589a22aecc
Robert Dickerson <rdickeriii@gmail.com>
2017-09-04 11:40:53 -0700

    Refactor: All the numbers and prints fo life.
    end_of_line

--- variables.py
+++ variables.py
@@ -2,7 +2,19 @@
 
 
 
-manila_pop = 1780148
-manila_area = foo
+manila_pop = 1788573
+manila_area = 16.56
 manila_pop_density = manila_pop / manila_area
+print(manila_area)
 print(manila_pop_density)
+
+
+manila_pop += 1675 # increase the value of manila_pop by 1675
+print(manila_pop)
+manila_pop -= 250 # decrease the value of manila_pop by 250
+print(manila_pop)
+manila_pop *= 0.9 # decimate manila_pop
+print(manila_area)
+manila_area /=  2 # approximate the female population of Manila
+print(manila_area)
+print(manila_pop)
