8b98d270360f4c6adaf3132d09a9fe946d48cb74
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 10:34:30 -0700

    Refactor: Meh
    end_of_line

--- ratings.py
+++ ratings.py
@@ -20,7 +20,8 @@ def convert_to_numeric(score):
     """
     Converts the scores into a numerical value
     """
-    return float(score)
+    converted_score = float(score)
+    return converted_score
 
 def sum_of_middle_three(score1, score2, score3, score4, score5):
     """
@@ -28,8 +29,8 @@ def sum_of_middle_three(score1, score2, score3, score4, score5):
     """
     max_score = max(score1, score2, score3, score4, score5)
     min_score = min(score1, score2, score3, score4, score5)
-    sum = score1 + score2 + score3 + score4 + score5 - max_score - min_score
-    return sum
+    total = score1 + score2 + score3 + score4 + score5 - max_score - min_score
+    return total
 
 
 def score_to_rating_string(av_score):
