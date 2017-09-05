4b6630da56a0d70edab42b56e668207e203cbd2a
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 11:31:23 -0700

    Refactor: Some More stuff. Got kinda bored
    end_of_line

--- working.py
+++ working.py
@@ -43,4 +43,25 @@ def some_ting():
     stuff = ["thing", "42", "nope"]
     return " and ".join(stuff)
 
-print(some_ting())
+print (some_ting())
+
+def top_three(input_list):
+    """Returns a list of the three largest elements input_list in order from largest to smallest.
+
+    If input_list has fewer than three elements, return input_list element sorted largest to smallest/
+    """
+    # TODO: implement this function
+
+def median(numbers):
+    numbers.sort()
+    if len(numbers) % 2:
+        # if the list has an odd number of elements,
+        # the median is the middle element
+        middle_index = int(len(numbers)/2)
+        return numbers[middle_index]
+    else:
+        # if the list has an even number of elements,
+        # the median is the average of the middle two elements
+        right_of_middle = len(numbers)//2
+        left_of_middle = right_of_middle - 1
+        return (numbers[right_of_middle] + numbers[left_of_middle])/2
