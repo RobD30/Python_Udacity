9c2ef2f8d1cca46cd96b945807b9842a0ea351e0
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 11:35:50 -0700

    Refactor: Starting with loops
    end_of_line

---
+++ looper.py
@@ -0,0 +1,8 @@
+def name_changer():
+    names = ['charlotte hippopotamus turner', 'oliver st. john-mollusc',
+             'nigel incubator-jones', 'philip diplodocus mallory']
+    for name in names:
+        print(name.title())
+
+
+print(name_changer())
