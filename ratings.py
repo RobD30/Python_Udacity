620746d3df2ca027fdd711465da5636ee4ecdb16
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 08:40:58 -0700

    Refactor: Instructions
    end_of_line

---
+++ ratings.py
@@ -0,0 +1,18 @@
+# Break the function down into 3-5 steps and write them in words in the box, in the order in which they will get
+# performed on the input data. Include the input and the output of each step. This answer will not get graded but we'll
+# share what we think afterwards. There is not a single right answer to a question like this, so just give it a try!
+# Your reflection
+# take in input, discard outliers, combine data, take the mean, output return
+# Things to think about
+# Thanks for thinking about this design decision!
+#
+# Here are our four steps:
+#
+# Take each of the raw scores and convert each of them to a numerical type.
+# Input: score as string/float/int. Output: score as float or int.
+# Choose the middle 3 values from the set of 5 scores.
+# Input: 5 scores as float or int. Output: 3 scores (same type).
+# Take the average of three scores.
+# Input: 3 scores as float or int. Output: 1 average score (float).
+# Choose the correct word rating for the average score.
+# Input: average score as float. Output: Rating as a string.
