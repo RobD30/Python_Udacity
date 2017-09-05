776c488998a2c023f544cfb01396658181389113
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 10:43:45 -0700

    Refactor: Eclipse list
    end_of_line

--- working.py
+++ working.py
@@ -9,3 +9,11 @@ def how_many_days(month_number):
 
 # This test case should print 31, the number of days in the eighth month, August
 print(how_many_days(8))
+
+eclipse_dates = ['June 21, 2001', 'December 4, 2002', 'November 23, 2003',
+                 'March 29, 2006', 'August 1, 2008', 'July 22, 2009',
+                 'July 11, 2010', 'November 13, 2012', 'March 20, 2015',
+                 'March 9, 2016']
+
+# Test for list
+print(eclipse_dates[7:10])
