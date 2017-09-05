5f50c4ad7af9fe31b583b11ed09f6161aea00e5c
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 08:25:12 -0700

    Refactor: Fixed which prize.
    end_of_line

--- if.py
+++ if.py
@@ -73,16 +73,19 @@ print(message)
 
 def which_prize(points):
     """
-    Returns the prize-winning message, given a number of points
+    Returns the number of prize-winning message, given a number of points
     """
+    prize = None
     if points <= 50:
-        return "Congratulations! You have won a wooden rabbit!"
-    elif points <= 150:
-        return "Oh dear, no prize this time."
-    elif points <= 180:
-        return "Congratulations! You have won a wafer-thin mint!"
+        prize = "a wooden rabbit"
+    elif 150 <= points <= 180:
+        prize = "a wafer-thin mint"
+    elif points >= 181
+        prize = "a penguin"
+    if prize:
+        return "Congratulations! You have won " + prize + "!"
     else:
-        return "Congratulations! You have won a penguin!"
+        return "Oh dear, no prize this time."
 
 def punctuate(sentence, phrase_type):
     """
