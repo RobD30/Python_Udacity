646ed8ec245f20303c72ad155d0eaa12d4b8d4cd
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 11:54:11 -0700

    Refactor: I getting it
    end_of_line

--- looper.py
+++ looper.py
@@ -6,3 +6,18 @@ def name_changer():
 
 
 print(name_changer())
+
+
+def list_sum(input_list):
+    total = 0
+    for element in input_list:
+        total += element
+    return total
+
+#These test cases check the list_sum works correctly
+test1 = list_sum([1, 2, 3])
+print("expected result: 6, actual result: {}".format(test1))
+
+test2 = list_sum([-1, 0, 1])
+print("expected result: 0, actual result: {}".format(test2))
+
