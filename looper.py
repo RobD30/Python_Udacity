bace7014acd77577e8c75adec3a6f23ce65b03c1
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 12:04:32 -0700

    Refactor: fucking dumb. They used count as a variable... assholes.
    end_of_line

--- looper.py
+++ looper.py
@@ -14,10 +14,10 @@ def list_sum(input_list):
         total += element
     return total
 
-#These test cases check the list_sum works correctly
+
+# These test cases check the list_sum works correctly
 test1 = list_sum([1, 2, 3])
 print("expected result: 6, actual result: {}".format(test1))
 
 test2 = list_sum([-1, 0, 1])
 print("expected result: 0, actual result: {}".format(test2))
-
