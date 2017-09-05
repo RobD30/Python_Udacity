ffe9d6d1ca3d50c3e0578733ff6f89b65942aa25
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 12:25:44 -0700

    Refactor: Moving On
    end_of_line

--- looper.py
+++ looper.py
@@ -29,15 +29,13 @@ def starbox(width, height):
     width: width of box in characters, must be at least 2
     height: height of box in lines, must be at least 2
     """
-    print("*" * width)  # print top edge of box
+    print("*" * width) #print top edge of box
 
     # print sides of box
-    # todo: print this line height-2 times, instead of three times
-    print("*" + " " * (width - 2) + "*")
-    print("*" + " " * (width - 2) + "*")
-    print("*" + " " * (width - 2) + "*")
+    for _ in range(height-2):
+        print("*" + " " * (width-2) + "*")
 
-    print("*" * width)  # print bottom edge of box
+    print("*" * width) #print bottom edge of box
 
 
 # Test Cases
@@ -46,3 +44,11 @@ starbox(5, 5)  # this prints correctly
 
 print("Test 2:")
 starbox(2, 3)  # this currently prints two lines too tall - fix it!
+
+card_deck = [4, 11, 8, 5, 13, 2, 8, 10]
+hand = []
+
+while sum(hand) <= 17:
+    hand.append(card_deck.pop())
+
+print(hand)
