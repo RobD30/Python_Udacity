5688b33a651ebf277dad3dca44f283ef478d216b
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 08:23:26 -0700

    Refactor: All the ifs and branchs
    end_of_line

--- if.py
+++ if.py
@@ -1,6 +1,6 @@
 if phone_balance < 10:
     phone_balance += 10
-bank_balance -= 10
+    bank_balance -= 10
 pass
 
 if 18.5 <= weight_in_kg / (height_in_m) ** 2 < 25:
@@ -80,6 +80,35 @@ def which_prize(points):
     elif points <= 150:
         return "Oh dear, no prize this time."
     elif points <= 180:
+        return "Congratulations! You have won a wafer-thin mint!"
+    else:
+        return "Congratulations! You have won a penguin!"
+
+def punctuate(sentence, phrase_type):
+    """
+    Create a punctuated sentence from a string. Defaults to an ordinary
+    sentence with a full stop.
+
+    sentence: string, the phrase that is to have punctuation added
+    phrase_type: string, defines what kind of sentence to create.
+                "exclamation", "question" and "aside" are known values.
+    """
+    if phrase_type == "exclamation":
+        sentence_punct = sentence + "!"
+    elif phrase_type == "question":
+        sentence_punct = sentence + "?"
+    elif phrase_type == "aside":
+        sentence_punct = "(" + sentence + ".)"
+    else:
+        sentence_punct = sentence + "."
+    return sentence_punct
+
+def which_pize():
+    if not prize:
+        return "Oh dear, no prize this time."
+    elif points <= 50:
+        return "Congratulatoins! You have won a wooden rabbit"
+    elif points >= 150 and <= 180:
         return "Congratulations! You have won a wafer-thin mint!"
     else:
         return "Congratulations! You have won a penguin!"
