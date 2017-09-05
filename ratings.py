8c5309f8a2ce2e875c3bd65d261bc6e915c1b5d0
Robert Dickerson <rdickeriii@gmail.com>
2017-09-05 09:12:47 -0700

    Refactor: Idk... This is what it said to do.
    end_of_line

--- ratings.py
+++ ratings.py
@@ -16,12 +16,23 @@
 # Input: 3 scores as float or int. Output: 1 average score (float).
 # Choose the correct word rating for the average score.
 # Input: average score as float. Output: Rating as a string.
-def scores_to_rating(score1,score2,score3,score4,score5):
+def scores_to_rating(score1, score2, score3, score4, score5):
     """
     Turns five scores into a rating by averaging the
     middle three of the five scores and assigning this average
     to a written rating.
     """
-    # THE CODE GOES HERE!
+    # Convert Scores to numbers
+    score1 = convert_to_numeric(score1)
+    score2 = convert_to_numeric(score2)
+    score3 = convert_to_numeric(score3)
+    score4 = convert_to_numeric(score4)
+    score5 = convert_to_numeric(score5)
+
+    # STEP 2 and STEP 3 find the average of the middle three scores
+    average_score = sum_of_middle_three(score1, score2, score3, score4, score5) / 3
+
+    # turn average score into a rating
+    rating = score_to_rating_string(average_score)
 
     return rating
